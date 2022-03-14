9385510a30306cf514602508ffe58befde6ff156
Dirceu Lopes <dirceu398@gmail.com>
2022-03-13 23:25:26 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -1,30 +1,25 @@
 package com.devsuperior.dsmovie.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
-import com.devsuperior.dsmovie.services.MovieService;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.services.ScoreService;
 
 @RestController
-@RequestMapping(value = "/movies")
+@RequestMapping(value = "/scores")
 public class MovieController {
 
 	@Autowired
-	private MovieService service;
+	private ScoreService service;
 	
-	@GetMapping
-	public Page<MovieDTO> findAll(Pageable pageable){
-		return service.findAll(pageable);
-	}
-	
-	@GetMapping(value = "/{id}")
-	public MovieDTO findById(@PathVariable Long id){
-		return service.findById(id);
+	@PutMapping
+	public MovieDTO SaveScore(@RequestBody ScoreDTO dto){
+		MovieDTO movieDTO = service.saveScore(dto);
+		return movieDTO;
 	}
 }
